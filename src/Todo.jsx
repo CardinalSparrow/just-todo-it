09dc21c9425fbd8c92b0baa7aab090155730c6fb
Cardinalsparrow <Cardinalsparrow@gmail.com>
2023-11-27 18:18:59 +0100

    Started with the jsx for the ui

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Todo = () => {
-  return <div></div>;
+  return (
+    <div id="container">
+      <div id="add-task-container">
+        <input id="input-task" type="text" />
+        <button id="add-task"></button>
+      </div>
+      <div id="task-container">
+        <div className="task">
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
