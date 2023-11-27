13e0e80bd984567164234778fe4d51f9587b97b3
Cardinalsparrow <Cardinalsparrow@gmail.com>
2023-11-27 18:33:48 +0100

    Most of the UI done

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./Todo.css";
 const Todo = () => {
   return (
     <div id="container">
       <div id="add-task-container">
+        <h3 className="msg error">Error message</h3>
         <input id="input-task" type="text" />
         <button id="add-task"></button>
       </div>
