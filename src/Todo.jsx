1984f6062969011340824fd390bb21e8e9fd8a23
Cardinalsparrow <Cardinalsparrow@gmail.com>
2023-11-28 13:08:19 +0100

    Added awesome fonts cdn and styled the buttons

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,15 +3,24 @@ import "./Todo.css";
 const Todo = () => {
   return (
     <div id="container">
+      <h1 id="title">
+        Just <span>to</span>do it <i className="fa-solid fa-check"></i>
+      </h1>
       <div id="add-task-container">
         <h3 className="msg error">Error message</h3>
         <input id="input-task" type="text" />
-        <button id="add-task"></button>
+        <button id="add-task">
+          <i className="fa-solid fa-plus"></i>
+        </button>
       </div>
       <div id="task-container">
         <div className="task">
-          <button></button>
-          <button></button>
+          <button>
+            <i className="fa-solid fa-check"></i>
+          </button>
+          <button className="deleteTask">
+            <i className="fa-solid fa-trash-can"></i>
+          </button>
         </div>
       </div>
     </div>
