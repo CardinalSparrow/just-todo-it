2f2fab6389c372df22a7afb4498629c9e6584cf4
Cardinalsparrow <Cardinalsparrow@gmail.com>
2023-11-28 15:24:13 +0100

    Tracking the task with the useState hook. declared a function to add task and passed it into the onClick eventsListener of the add task button

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Todo.css";
 const Todo = () => {
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState("00:00");
+  const [error, setError] = useState("");
+  const [allTasks, setAllTasks] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+    return JSON.parse(localValue);
+  });
+  const addTask = () => {
+    if (task.trim() !== "") {
+      const newTask = {
+        text: task,
+        completed: false,
+        date: new Date().toLocaleDateString(),
+        time: time,
+      };
+      setAllTasks([...allTasks, newTask]);
+      setTask("");
+      setError("");
+      setTime("00:00");
+    } else {
+      setError("Please enter a task!");
+    }
+  };
   return (
     <div id="container">
       <h1 id="title">
         Just <span>to</span>do it <i className="fa-solid fa-check"></i>
       </h1>
       <div id="add-task-container">
-        <h3 className="msg error">Error message</h3>
-        <input id="input-task" type="text" />
-        <button id="add-task">
+        {error && <h3 className="msg error">{error}</h3>}
+        <input
+          id="input-task"
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task"
+          type="text"
+          value={task}
+        />
+        <button id="add-task" onClick={addTask}>
           <i className="fa-solid fa-plus"></i>
         </button>
       </div>
       <div id="task-container">
         <div className="task">
-          <button>
+          <button className="checkTask">
             <i className="fa-solid fa-check"></i>
           </button>
           <button className="deleteTask">
